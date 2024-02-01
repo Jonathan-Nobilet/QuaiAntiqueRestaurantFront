638397eb5eba801efd4d7fffd2cf265969166389
jonathan nobilet <jonathan.nobilet@outlook.fr>
2024-02-01 17:39:23 +0100

    ajout de la page galerie

--- router/allRoutes.js
+++ router/allRoutes.js
@@ -3,7 +3,7 @@ import Route from "./Route.js";
 //Définir ici vos routes
 export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html"),
-    new Route("/Galerie", "Galerie", "/pages/Galerie.html"),
+    new Route("/galerie", "galerie", "/Pages/galerie.html"),
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
